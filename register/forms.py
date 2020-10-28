ed46c4516ed322865653291f7145ab19644a7f58
Thomas Chastaingt <thomas-chastaingt@outlook.com>
2020-10-28 13:23:23 +0100

    add modal update and informations

--- register/forms.py
+++ register/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
+#custom register form with differents fields
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -11,9 +12,10 @@ class RegisterForm(UserCreationForm):
         fields = ["username", "email", "password1", "password2"]
 
 
+#custom edit profil with only email
 class EditForm(UserChangeForm):
     email = forms.EmailField()
-
+    
     class Meta:
         model = User
         fields = ["email"]
--- register/views.py
+++ register/views.py
@@ -3,7 +3,7 @@ from .forms import RegisterForm, EditForm
 from django.views import View
 from django.http import HttpResponseRedirect
 
-
+# class to show register template and to submit a new user
 class RegisterView(View):
     form = RegisterForm
     template_name = 'register/register.html'
@@ -20,11 +20,19 @@ class RegisterView(View):
         
         return render(request, self.template_name, {'form': form})
 
-
+#class to edit your own informations
 class EditUserView(View):
     form = EditForm
     template_name = 'edit/edit_user.html'
 
     def get(self, request, *args, **kwargs):
         form = self.form
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return render(request, self.template_name, {'form': form})
+        
         return render(request, self.template_name, {'form': form})
